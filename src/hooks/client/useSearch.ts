36209bf599e380327d487fdcdb55e74b70b490ef
Ross Igolkin <ross.igolkin@gmail.com>
2022-04-21 22:11:04 +0300

    add prefetch to list

--- src/hooks/client/useSearch.ts
+++ src/hooks/client/useSearch.ts
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
-import { useQuery } from "react-query";
+import { useQuery, QueryClient, useQueryClient } from "react-query";
 import { useDispatch } from "react-redux";
 import { List, UseClientListProps } from "types";
 import { setIsFetching } from "uiSlice";
 import Client from "../../services/Client";
 
-export function useSearch<T>({ path, offset = 0, limit, queryOptions = {}, reqOptions }: UseClientListProps<T>) {
+export function useSearch<T>({ path, offset = 0, limit = 0, queryOptions = {}, reqOptions }: UseClientListProps<T>) {
   const query = useQuery<List<T> | undefined, Error>(
     [path, offset],
     ({ signal }) =>
@@ -14,6 +14,19 @@ export function useSearch<T>({ path, offset = 0, limit, queryOptions = {}, reqOp
   );
   const dispatch = useDispatch();
 
+  const queryClient = useQueryClient();
+
+  const prefetchSearch = async () => {
+    // The results of this query will be cached like a normal query
+    await queryClient.prefetchQuery([path, offset + limit], ({ signal }) =>
+      Client.search(path, {
+        signal,
+        ...reqOptions,
+        params: { offset: offset + limit, limit: limit, ...reqOptions?.params },
+      })
+    );
+  };
+
   useEffect(() => {
     dispatch(setIsFetching(query.isFetching && !query.isLoading));
 
@@ -21,6 +34,12 @@ export function useSearch<T>({ path, offset = 0, limit, queryOptions = {}, reqOp
       dispatch(setIsFetching(false));
     };
   }, [dispatch, query.isFetching, query.isLoading]);
+
+  useEffect(() => {
+    if (query.data?.count && query.data?.count > offset + limit) {
+      prefetchSearch();
+    }
+  }, [query.data?.count, offset, limit]);
 
   return query;
 }
